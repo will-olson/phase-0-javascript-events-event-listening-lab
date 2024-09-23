ae0a55edd216b4ccfbf313835ad585a1da28911c
Will Olson <will.olson.b@gmail.com>
2024-09-23 00:45:26 -0500

    Updated index.js to resolve error.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  // Select the element with ID 'button'
+  const button = document.getElementById('button');
+
+  // Define the event listener function
+  function handleClick() {
+    alert('Button was clicked!');
+  }
+
+  // Bind the event listener to the button
+  button.addEventListener('click', handleClick);
 }
